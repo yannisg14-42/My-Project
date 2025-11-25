f15a8cd3a5539f8fcaa2449bc18c5d80b2daa78b
yannisg14-42 <yannisg14@gmail.com>
2025-11-25 19:55:48 +0100

    Add files via upload

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: sgarba <sgarba@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 16:33:24 by sgarba            #+#    #+#             */
-/*   Updated: 2025/11/25 16:36:38 by sgarba           ###   ########.fr       */
+/*   Updated: 2025/11/25 19:15:45 by sgarba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 void  ft_bzero(void *s, size_t n)
 {
-  size_t  i;
-  char	*str;
-  
-  i = 0;
-  while (i <= n-1)
-  {
-    str[i] = '\0';
-    i++;
-  }
+ 	size_t	i;
+ 	char	*str;
+ 
+  	str = (char *)s; // how to cast the void pointer
+  	i = 0;
+  	while (i <= n-1)
+  	{
+    		str[i] = '\0';
+    		i++;
+  	}
 }
 // ft_bzero place n zero-valued bytes in the area pointed to by s
-// void *s means generix pointer so it can point to any type of variable
+// void *s means generic pointer, so it can point to any type of variable
 
 #include <stdio.h>
 
 int  main(int argc, char **argv)
 {
-  char  *str;
-
-  if (argc == 2)
-  {
-    str = ft_bzero(argv[1], 10);
-  }
-  return (0);
+	if (argc == 2)
+	{
+		ft_bzero(argv[1], 10);
+		printf("after bzero: %s\n", argv[1]);
+	}
+	return (0);
 }
