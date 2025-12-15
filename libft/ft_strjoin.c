71caab24fc8f87314efe949c1cea23cc9d408e68
yannisg14-42 <sgarba@student.42berlin.de>
2025-12-15 17:41:37 +0100

    Refactor null checks and variable declarations

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -17,19 +17,17 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new_str;
-	size_t	total_len;
-	size_t	s1_len;
-	size_t	s2_len;
+	size_t	total_len, s1_len, s2_len;
 
-	if (s1 == NULL)
+	if (!s1)
 		s1 = "";
-	if (s2 == NULL)
+	if (!s2)
 		s2 = "";
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	total_len = s1_len + s2_len;
 	new_str = malloc(total_len + 1);
-	if(new_str == NULL)
+	if(!new_str)
 		return (NULL);
 	ft_memcpy(new_str, s1, s1_len);
 	ft_memcpy((new_str + s1_len), s2, s2_len);
