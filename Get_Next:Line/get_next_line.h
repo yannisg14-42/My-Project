8b6532cd91b6b7cb8d71a906f7803f8321109129
yannisg14-42 <sgarba@student.42berlin.de>
2026-03-17 03:31:36 +0100

    Update get_next_line.h

--- Get_Next:Line/get_next_line.h
+++ Get_Next:Line/get_next_line.h
@@ -7,15 +7,15 @@
 # include <stddef.h>
 # include <unistd.h>
 
-size_t    ft_strlen(const char *s);
+size_t   ft_strlen(const char *s);
 
-void    *ft_memchr(const void *ptr, int c, size_t n);
+void     *ft_memchr(const void *ptr, int c, size_t n);
 
-char    *ft_extract_new_line(char *buff, size_t newline_position);
+char     *ft_extract_new_line(char *buff, size_t newline_position);
 
-char    *ft_shift_buff(char *buff, size_t newline_position);
+char     *ft_shift_buff(char *buff, size_t newline_position);
 
-char    *ft_append_buff(char *buff, char *tmp_buff, size_t tmp_size);
+char     *ft_append_buff(char *buff, char *tmp_buff, size_t tmp_size);
 
 ssize_t  ft_read_buff(int fd, char **static_buff);
 
