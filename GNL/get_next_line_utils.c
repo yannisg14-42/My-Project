64f84cff32fd87ce459c2b209fd9034a2b411c70
yannisg14-42 <yannisg14@gmail.com>
2026-03-20 20:54:42 +0100

    Add files via upload

---
+++ GNL/get_next_line_utils.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgarba <sgarba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 13:41:44 by sgarba            #+#    #+#             */
+/*   Updated: 2026/03/18 14:35:19 by sgarba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Get the length of a string */
+#include "get_next_line.h"
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/* Find the \n character or return NULL*/
+void	*ft_memchr(const void *ptr, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	str = (unsigned char *)ptr;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+/* Extract the line with \n in it*/
+char	*ft_extract_new_line(char *static_buff, size_t newline_position)
+{
+	char	*line;
+	size_t	i;
+
+	line = malloc(newline_position + 2);
+	i = 0;
+	if (!line)
+		return (NULL);
+	while (i <= newline_position)
+	{
+		line[i] = static_buff[i];
+		i++;
+	}
+	line[newline_position + 1] = '\0';
+	return (line);
+}
+
+/* Shift the static buffer to only contain what commes after \n*/
+char	*ft_shift_buff(char *static_buff, size_t newline_position)
+{
+	char	*after_newline;
+	size_t	i;
+	size_t	after_newline_len;
+
+	after_newline_len = ft_strlen(static_buff) - (newline_position + 1);
+	if (!after_newline_len)
+	{
+		free(static_buff);
+		return (NULL);
+	}
+	after_newline = malloc(after_newline_len + 1);
+	i = newline_position + 1;
+	if (!after_newline)
+	{
+		free(static_buff);
+		return (NULL);
+	}
+	while (static_buff[i])
+	{
+		after_newline[i - (newline_position + 1)] = static_buff[i];
+		i++;
+	}
+	after_newline[after_newline_len] = '\0';
+	free (static_buff);
+	return (after_newline);
+}
+
+/* Apend tmp buff to static buff*/
+char	*ft_append_buff(char *static_buff, char *tmp_buff, size_t tmp_size)
+{
+	char	*full_buff;
+	size_t	static_bufflen;
+	size_t	i;
+
+	static_bufflen = 0;
+	if (static_buff)
+		static_bufflen = ft_strlen(static_buff);
+	full_buff = malloc(static_bufflen + tmp_size + 1);
+	if (!full_buff)
+	{
+		free(static_buff);
+		return (NULL);
+	}
+	i = 0;
+	while (i < (static_bufflen + tmp_size))
+	{
+		if (i < static_bufflen)
+			full_buff[i] = static_buff[i];
+		else
+			full_buff[i] = tmp_buff[i - static_bufflen];
+		i++;
+	}
+	full_buff[static_bufflen + tmp_size] = '\0';
+	free(static_buff);
+	return (full_buff);
+}
