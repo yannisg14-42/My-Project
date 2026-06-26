c8bfc7b57ccd9e2e5bf88c79a948efa706d7777f
yannisg14-42 <yannisg14@gmail.com>
2026-06-27 00:33:56 +0200

    Add files via upload

--- Python_Module_00/ft_seed_inventory.py
+++ Python_Module_00/ft_seed_inventory.py
@@ -1,2 +1,11 @@
-def ft_seed_inventory(see_type: str, quantity: int, unit: str) -> None:
-    
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    if unit == "packets":
+        print(seed_type.capitalize(), "seeds:", quantity, unit, "available")
+    elif unit == "grams":
+        print(seed_type.capitalize(), "seeds:", quantity, unit, "total")
+    elif unit == "area":
+        print(seed_type.capitalize(), "seeds: covers", quantity, "square meters")
+    else:
+        print("Unknown unit type")
+
+
