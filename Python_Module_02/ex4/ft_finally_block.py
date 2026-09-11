cacfc4f040d4e9b174b5695591d781fe18a43617
Yannis <sgarba@student.42berlin.de>
2026-09-12 01:10:41 +0200

    Almost done with P_M_02

---
+++ Python_Module_02/ex4/ft_finally_block.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# >>>>Global Constants<<<<
+
+HEADER: str = "=== Garden Watering System ===\n"
+PROGRAM_END: str = "Cleanup always happens, even with errors!"
+
+
+def water_plant(plant_name: str) -> None:
+
+    """
+    
+    """
+
+    
